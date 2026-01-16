aae6ad3ad03ea0a97d3ade71ac4d2a367b02c03f
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-01-16 18:23:09 +0700

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+      <div className="letter-wrapper">
+        <span className="loader-letter">L</span>
+        <span className="loader-letter">o</span>
+        <span className="loader-letter">a</span>
+        <span className="loader-letter">d</span>
+        <span className="loader-letter">i</span>
+        <span className="loader-letter">n</span>
+        <span className="loader-letter">g</span>
+        <span className="loader-letter">.</span>
+        <span className="loader-letter">.</span>
+        <span className="loader-letter">.</span>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
