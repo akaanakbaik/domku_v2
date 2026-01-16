a6fb02534b4e4d7be5514a595388d75b9230b56f
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-01-16 19:28:12 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import './index.css' // Import CSS wajib paling atas
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-console.log("React App is mounting..."); // Cek Console Browser jika masih blank
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
