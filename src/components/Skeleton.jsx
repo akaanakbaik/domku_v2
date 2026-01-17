e910b7f4345a70ac0d2f90b50034b1c41aa25fbe
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-01-17 10:49:31 +0700

    Update Skeleton.jsx

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 
 const Skeleton = ({ className }) => {
   return (
-    <div className={`bg-blue-900/10 animate-pulse rounded-lg ${className}`}></div>
+    <div className={`animate-pulse bg-blue-900/20 rounded-lg ${className}`}></div>
   )
 }
 
-export const TableSkeleton = () => (
-  <div className="space-y-3">
-    {[1, 2, 3].map((i) => (
-      <div key={i} className="bg-[#111318] border border-blue-900/10 p-4 rounded-xl flex items-center justify-between">
-        <div className="space-y-2 w-full">
-          <Skeleton className="h-4 w-1/3" />
-          <Skeleton className="h-3 w-1/4" />
+export const DashboardSkeleton = () => {
+  return (
+    <div className="space-y-4">
+      {[1, 2, 3].map((i) => (
+        <div key={i} className="bg-[#111318] border border-blue-900/20 rounded-xl p-4 flex flex-col md:flex-row justify-between gap-4">
+          <div className="space-y-2 w-full">
+            <Skeleton className="h-6 w-1/3" />
+            <div className="flex gap-2">
+              <Skeleton className="h-4 w-12" />
+              <Skeleton className="h-4 w-1/2" />
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="h-10 w-10" />
+            <Skeleton className="h-10 w-10" />
+          </div>
         </div>
-        <Skeleton className="h-8 w-8 rounded-lg" />
-      </div>
-    ))}
-  </div>
-)
+      ))}
+    </div>
+  )
+}
 
 export default Skeleton
