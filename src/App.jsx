a61b18464754c0e42cfaded23d1d3b154cd0e8e5
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-01-18 11:24:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import VerifyEmail from './pages/VerifyEmail'
+import ResetPassword from './pages/ResetPassword'
 import Dashboard from './pages/Dashboard'
 import Settings from './pages/Settings'
 import ApiDocs from './pages/ApiDocs'
+import NotFound from './pages/NotFound'
 import Layout from './components/Layout'
 
 function App() {
@@ -15,9 +17,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/subdomain" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/api" element={<ApiDocs />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
